d33ad0eae91431e08beed39f3850f6d4c6f5c8cd
Ted Kim <ted.s.kim1004@gmail.com>
2020-08-27 10:04:50 -0700

    Added Lindsey's code.

--- phase3/code/java/src/MechanicShop.java
+++ phase3/code/java/src/MechanicShop.java
@@ -340,15 +340,318 @@ public class MechanicShop{
 	}
 	
 	public static void AddMechanic(MechanicShop esql){//2 Lindsey
-		
+			String mechFirstName;
+			String mechLastName;
+			int mechID = 0;
+			int mechExp; 
+
+			// Reading in input using Scanner
+			Scanner scnr = new Scanner(System.in);
+
+			while(true){
+				try{
+					System.out.print("Enter the mechanic first name: ");
+					mechFirstName = scnr.nextLine();
+					if(mechFirstName.length() > 32){
+						throw new Exception("First name cannot exceed 32 characters.");
+					}
+					else if(mechFirstName.length() <= 0){
+						throw new Exception("First name cannot be null.");
+					}
+					break;
+				}
+				catch(Exception ex){
+					System.out.println(ex.getMessage());
+					System.out.println("Please try again.");
+				}
+			}
+
+			while(true){
+				try{
+					System.out.print("Enter the mechanic last name: ");
+					mechLastName = scnr.nextLine();
+					if(mechLastName.length() > 32){
+						throw new Exception("Last name cannot exceed 32 characters.");
+					}
+					else if(mechLastName.length() <= 0){
+						throw new Exception("Last name cannot be null.");
+					}
+					break;
+				}
+				catch(Exception ex){
+					System.out.println(ex.getMessage());
+					System.out.println("Please try again.");
+				}
+
+			}
+
+			try{
+				String qs;
+				List<List<String>> x;
+				qs = String.format("SELECT M.id FROM Mechanic M");
+				x = esql.executeQueryAndReturnResult(qs);
+				mechID = x.size() + 1;
+			}
+			catch(Exception e){
+				System.err.println (e.getMessage());
+			}
+
+			while(true){
+				try{
+					System.out.print("Enter the mechanic's years of experience: ");
+					mechExp = scnr.nextInt(); 
+					if(mechExp < 0){
+						throw new Exception("Years of experience cannot be a negative number.");
+					}
+					break;
+				}
+				catch(Exception ex){
+					System.out.println(ex.getMessage());
+					System.out.println("Please try again.");
+				}
+			}
+			// Putting values into database: 
+			try{
+				System.out.println("\nAdding new mechanic to database:");
+				esql.executeUpdate("INSERT INTO MECHANIC (id, fname, lname, experience) VALUES (" 
+				+ mechID + ", '" + mechFirstName + "', '" + mechLastName + "', " + mechExp + ");"  );
+				// This will output the newly entered data:
+				String test = "SELECT * FROM MECHANIC WHERE id = '" + mechID + "';" ;
+				esql.executeQueryAndPrintResult(test);
+				System.out.println("Done adding mechanic. Returning to MAIN MENU...\n");
+				
+			}
+			catch (Exception e){
+				System.err.println (e.getMessage());
+			}
+
 	}
 	
 	public static void AddCar(MechanicShop esql){//3 Lindsey
+				String carVin;
+		String carMake;
+		String carModel;
+		int carYear; 
 		
+		// Reading in input using Scanner
+		Scanner scnr = new Scanner(System.in);
+
+		while(true){
+			try{
+				System.out.print("Enter the car vin: ");
+				carVin = scnr.next();
+				if(carVin.length() == 0 || carVin.length() > 16){
+					throw new Exception("Invalid VIN, can't be null and must be less than 16 characters.");
+				}
+				
+				System.out.print("Enter the car make: ");
+				carMake = scnr.next();
+				if(carMake.length() == 0 || carMake.length() > 32){
+					throw new Exception("Invalid input. Cannot exceed 32 characters!");
+				}
+				
+				System.out.print("Enter the car model: ");
+				carModel = scnr.next();
+				if(carModel.length() == 0 || carModel.length() > 32){
+					throw new Exception("Invalid input. Cannot exceed 32 characters!");
+				}
+				
+				System.out.print("Enter the car year: ");
+				carYear = scnr.nextInt();
+				if(carYear < 1900 || carYear > 2020){
+					throw new Exception("Invalid input. Must be a valid year.");
+				}
+				
+				// input values into db
+				System.out.println("\nAdding new car to database:");
+				esql.executeUpdate("INSERT INTO CAR (vin, make, model, year) VALUES ('" 
+				+ carVin + "', '" + carMake + "', '" + carModel + "', " + carYear + ");"  );
+
+				//show that values are in db
+				String test = "SELECT * FROM CAR WHERE vin = '" + carVin + "';" ;
+				esql.executeQueryAndPrintResult(test);
+				System.out.println("Done adding car. Returning to MAIN MENU...\n");
+				break;
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 	
 	public static void InsertServiceRequest(MechanicShop esql){//4 Lindsey
-		
+		String userInput;
+		boolean notValid = true;
+		Scanner scnr = new Scanner(System.in);
+		String sql = "";
+
+		// ask for user to input the last name, read in input
+		System.out.print("Enter in the last name: ");
+		userInput = scnr.next();
+		int test = -1;
+		boolean keepAsking = true;
+
+		// check if exists in table
+		try{
+			String check = "SELECT * FROM CUSTOMER WHERE lname= '" + userInput + "';" ;
+			test = esql.executeQuery(check);
+			//System.out.println("Test val: " + test); // for testing, comment out later
+			// if test is 1, means that record exists. 0 means doesnt exist in table
+		}
+		catch (Exception e){
+			System.err.println (e.getMessage());
+		}
+
+		if(test >= 1){ // record exists in table
+			// output all clients that match user's input
+			try{
+				String getLName = "SELECT id, fname, lname FROM CUSTOMER WHERE lname = '" + userInput + "';" ;
+				esql.executeQueryAndPrintResult(getLName);
+			}
+			catch (Exception e){
+				System.err.println (e.getMessage());
+			}
+			
+			int custID = -1;
+
+			while(notValid){
+				while(notValid){
+					System.out.print("\n Enter in the id of customer you would like to select: ");
+					custID = scnr.nextInt();
+						//check if selection exists (is valid)
+					try{
+						test = esql.executeQuery("SELECT id, fname, lname FROM CUSTOMER WHERE lname = '" + userInput + "' AND id = " + custID + ";");
+					}
+					catch (Exception e){
+						System.err.println (e.getMessage());
+					}
+					if(test == 1){// means its valid customer id
+						notValid = false;
+					}
+					else{ // means its not a valid customer id
+						System.out.println("Invalid customer id. Please try again.");
+						notValid = true;
+					}
+				}
+
+				String getCars = "SELECT ownership_id, vin, make, model, year FROM OWNS o, Customer cust, Car cc WHERE o.customer_id = cust.id AND o.car_vin = cc.vin AND cust.id = " + custID + ";" ;
+				//check if selection exists (is valid)
+				try{
+					test = esql.executeQuery(getCars);
+				}
+				catch (Exception e){
+					System.err.println (e.getMessage());
+				}
+				// if test is >= 1, means record(s) exists. 0 means doesnt exist in table
+				int ownID;
+				if(test >= 1){
+					while(true){
+						try{
+							esql.executeQueryAndPrintResult(getCars);
+						}
+						catch (Exception e){
+							System.err.println (e.getMessage());
+						}
+						System.out.print("\nEnter in the ownership id of the car you would like to add the service request to: ");
+						ownID = scnr.nextInt();
+						try{
+							test = esql.executeQuery("SELECT ownership_id, vin, make, model, year FROM OWNS o, Customer cust, Car cc WHERE o.customer_id = cust.id AND o.car_vin = cc.vin AND cust.id = " + custID + "AND ownership_id = " + ownID + ";");
+						    
+						}
+						catch (Exception e){
+							System.err.println (e.getMessage());
+						}
+						if(test == 1){//valid car selection
+							// Initiate the service request here
+							System.out.println("Initiating service request...");
+
+							try{
+							// getting rid
+								String qs1;
+								List<List<String>> x;
+								int setRid;
+								qs1 = String.format("SELECT s.rid FROM Service_request s");
+								x = esql.executeQueryAndReturnResult(qs1);
+								setRid = x.size() + 1;
+							
+							// getting VIN
+								String qs2;
+								List<List<String>> y;
+								String setVin;
+								qs2 = String.format("SELECT o.car_vin FROM Owns o WHERE o.ownership_id = '" + ownID + "';");
+								y = esql.executeQueryAndReturnResult(qs2);
+								setVin = y.toString();
+								setVin = setVin.substring(2, setVin.length()-2);
+							
+							// getting odometer
+								int odo = 0;
+								System.out.print("Enter odometer reading: ");
+								odo = scnr.nextInt();
+								if(odo <= 0){
+									throw new Exception("Odometer can't be null.");
+								}
+							// getting complaint 	
+								String comp = "";
+								System.out.print("Enter complaint: ");
+								scnr.nextLine();
+								comp = scnr.nextLine();
+								if(comp.length() >= 10000){
+									throw new Exception("Too long.");
+								}								
+							
+								try{
+									System.out.println("\nCreated Service Request:");
+									sql = "INSERT INTO SERVICE_REQUEST (rid, customer_id, car_vin, date, odometer, complain) VALUES (" + setRid + ", " 
+									+ custID + ", '" + setVin + "', " + "CURRENT_DATE," + odo + ", '" + comp + "');";
+									esql.executeUpdate(sql);
+									String t = "SELECT * FROM SERVICE_REQUEST WHERE rid = '" + setRid + "';" ;
+									esql.executeQueryAndPrintResult(t);
+									System.out.println("Done making service request.\n");
+									break;
+								}
+								catch(Exception e){
+									System.err.println (e.getMessage());
+								}
+							
+							}
+							catch(Exception e){
+								System.err.println (e.getMessage());
+							}
+						}
+						else{// invalid car selection
+							System.out.println("Invalid car selection. Try again.\n");
+						}
+					}
+
+				}
+				else{
+					System.out.println("Car doesn't exist.");
+				}
+			
+			}
+
+		}
+		else{ // record does not exist, option to add customer
+			int userChoice;
+			System.out.println("Customer does not exist in database.");
+			
+			while(keepAsking){
+				System.out.println("Would you like to add this customer to the database? \n 1. Yes \n 2. No");
+				userChoice = scnr.nextInt();
+				if(userChoice == 1){ // go to AddCustomer function
+					AddCustomer(esql);
+					break;
+				}
+				else if(userChoice == 2){ // Exit back to Main Menu
+					System.out.println("Stopped initiating service request... Returning to MAIN MENU.");
+					break;
+				}
+				else{
+					System.out.println("Invalid choice. Please try again.");
+					keepAsking = true;
+				}
+			}				
+		}
 	}
 	
 	public static void CloseServiceRequest(MechanicShop esql) throws Exception{//5 Ted
@@ -453,12 +756,31 @@ public class MechanicShop{
 	
 	public static void ListKCarsWithTheMostServices(MechanicShop esql){//9 Lindsey
 		//
-		
+			int k;
+		Scanner scnr = new Scanner(System.in);
+
+		System.out.print("Enter K number of cars: ");
+		k = scnr.nextInt();
+
+		String sqlCmd = "SELECT cc.make, cc.model, COUNT(*) AS num_serv_requests FROM CAR cc, Service_request s WHERE cc.vin = s.car_vin GROUP BY cc.vin ORDER BY num_serv_requests desc LIMIT " + k + ";" ;
+
+		try{
+			esql.executeQueryAndPrintResult(sqlCmd);
+		}
+		catch (Exception e){
+			System.err.println (e.getMessage());
+		}
 	}
 	
 	public static void ListCustomersInDescendingOrderOfTheirTotalBill(MechanicShop esql){// 10 Lindsey
 		//
+		String sqlCmd = "SELECT cust.fname, cust.lname, SUM (bill) AS total_bill FROM CUSTOMER cust, SERVICE_REQUEST sr, CLOSED_REQUEST cr WHERE cust.id = sr.customer_id AND cr.rid = sr.rid GROUP BY cust.id ORDER BY total_bill desc;" ;
 		
-	}
+		try{
+			esql.executeQueryAndPrintResult(sqlCmd);
+		}
+		catch (Exception e){
+			System.err.println (e.getMessage());
+		}
 	
 }
