9b7691423b4f22c0081c67dec8a1160392d9165a
Lindsey Young <lindsey@Lindseys-MacBook-Pro.local>
2020-08-27 02:18:12 -0700

    attempt at error handling for addMechanic

--- phase3/code/java/src/MechanicShop.java
+++ phase3/code/java/src/MechanicShop.java
@@ -315,60 +315,51 @@ public class MechanicShop{
 		// has basic adding to db functionality
 		// still need to implement checking valid parameters part 
 		// aka error handling if wrong input
+		boolean keepAsking = true;
 
-		int testId;
-		try{
-			testId =  esql.getCurrSeqVal("Select id From Mechanic;");
-			System.out.println("Test id: " + testId);
-		}
-		catch (Exception e){
-			System.err.println (e.getMessage());
-		}
-		
-
-		
-		String mechFirstName;
-		String mechLastName;
-		int mechID;
-		int mechExp; 
-		
-		// Reading in input using Scanner
-		Scanner scnr = new Scanner(System.in);
-
-		System.out.print("Enter the mechanic first name: ");
-		mechFirstName = scnr.nextLine();
-		System.out.print("Enter the mechanic last name: ");
-		mechLastName = scnr.nextLine();
-		System.out.print("Enter the mechanic id: ");
-		mechID = scnr.nextInt();
-		System.out.print("Enter the mechanic's years of experience: ");
-		mechExp = scnr.nextInt();
-
-		//	Testing code to make sure it is reading in input correctly:
-		// System.out.println("First name: " + mechFirstName);
-		// System.out.println("Last name: " + mechLastName);
-		// System.out.println("Mechanic ID: " + mechID);
-		// System.out.println("Years experience: " + mechExp);
-		
-		// Putting values into database: 
-		try{
-			esql.executeUpdate("INSERT INTO MECHANIC (id, fname, lname, experience) VALUES (" 
-			+ mechID + ", '" + mechFirstName + "', '" + mechLastName + "', " + mechExp + ");"  );
-		}
-		catch (Exception e){
-			System.err.println (e.getMessage());
-		}
+		while(keepAsking) {
+			String mechFirstName;
+			String mechLastName;
+			int mechID;
+			int mechExp; 
 			
-		
-		// Check if values got inputted into DB
-		try{
-			String test = "SELECT * FROM MECHANIC WHERE id = '" + mechID + "';" ;
-			esql.executeQueryAndPrintResult(test);
+			// Reading in input using Scanner
+			Scanner scnr = new Scanner(System.in);
+
+			System.out.print("Enter the mechanic first name: ");
+			mechFirstName = scnr.nextLine();
+			System.out.print("Enter the mechanic last name: ");
+			mechLastName = scnr.nextLine();
+			System.out.print("Enter the mechanic id: ");
+			mechID = scnr.nextInt();
+			System.out.print("Enter the mechanic's years of experience: ");
+			mechExp = scnr.nextInt();
+
+			//	Testing code to make sure it is reading in input correctly:
+			// System.out.println("First name: " + mechFirstName);
+			// System.out.println("Last name: " + mechLastName);
+			// System.out.println("Mechanic ID: " + mechID);
+			// System.out.println("Years experience: " + mechExp);
+			
+			// Putting values into database: 
+			try{
+				esql.executeUpdate("INSERT INTO MECHANIC (id, fname, lname, experience) VALUES (" 
+				+ mechID + ", '" + mechFirstName + "', '" + mechLastName + "', " + mechExp + ");"  );
+			}
+			catch (Exception e){
+				System.err.println (e.getMessage());
+			}
+				
+			
+			// Check if values got inputted into DB
+			try{
+				String test = "SELECT * FROM MECHANIC WHERE id = '" + mechID + "';" ;
+				esql.executeQueryAndPrintResult(test);
+			}
+			catch (Exception e){
+				System.err.println (e.getMessage());
+			}
 		}
-		catch (Exception e){
-			System.err.println (e.getMessage());
-		}
-		
 
 
 	}
