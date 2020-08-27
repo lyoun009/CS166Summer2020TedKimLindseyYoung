d693025332f184bec79bd97a330e88a9b0577257
Ted Kim <ted.s.kim1004@gmail.com>
2020-08-27 04:01:43 -0700

    Forgot semicolon

--- phase3/code/java/src/MechanicShop.java
+++ phase3/code/java/src/MechanicShop.java
@@ -23,7 +23,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Scanner
+import java.util.Scanner;
 
 /**
  * This class defines a simple embedded SQL utility class that is designed to
